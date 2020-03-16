10b2a3f23d80a925d0e8645958997205c6d7b15b
alicia65 <alicia65>
2020-03-16 09:50:55 -0500

    Add project files.

---
+++ Geography Quiz/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Geography_Quiz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblOceanIsTheLargest = new System.Windows.Forms.Label();
+            this.txtAnswer = new System.Windows.Forms.TextBox();
+            this.btnCheckAnswer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblOceanIsTheLargest
+            // 
+            this.lblOceanIsTheLargest.Location = new System.Drawing.Point(45, 30);
+            this.lblOceanIsTheLargest.Name = "lblOceanIsTheLargest";
+            this.lblOceanIsTheLargest.Size = new System.Drawing.Size(228, 17);
+            this.lblOceanIsTheLargest.TabIndex = 0;
+            this.lblOceanIsTheLargest.Text = "Which ocean is the largest?";
+            this.lblOceanIsTheLargest.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblOceanIsTheLargest.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtAnswer
+            // 
+            this.txtAnswer.Location = new System.Drawing.Point(48, 96);
+            this.txtAnswer.Name = "txtAnswer";
+            this.txtAnswer.Size = new System.Drawing.Size(100, 22);
+            this.txtAnswer.TabIndex = 1;
+            // 
+            // btnCheckAnswer
+            // 
+            this.btnCheckAnswer.Location = new System.Drawing.Point(285, 95);
+            this.btnCheckAnswer.Name = "btnCheckAnswer";
+            this.btnCheckAnswer.Size = new System.Drawing.Size(75, 23);
+            this.btnCheckAnswer.TabIndex = 2;
+            this.btnCheckAnswer.Text = "Check";
+            this.btnCheckAnswer.UseVisualStyleBackColor = true;
+            this.btnCheckAnswer.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(507, 167);
+            this.Controls.Add(this.btnCheckAnswer);
+            this.Controls.Add(this.txtAnswer);
+            this.Controls.Add(this.lblOceanIsTheLargest);
+            this.Name = "Form1";
+            this.Text = "Geography Quiz";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblOceanIsTheLargest;
+        private System.Windows.Forms.TextBox txtAnswer;
+        private System.Windows.Forms.Button btnCheckAnswer;
+    }
+}
+
---
+++ Geography Quiz/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Geography_Quiz
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string answer = txtAnswer.Text;
+            answer = answer.ToLower();
+
+            int oceanCount = 5;
+            string oceanInfo = $"There are {oceanCount} ocean on earth.";
+            //"There are 5 oceans on earth."
+
+            string ocean = "Pacific Ocean";
+            double oceanSize = 63800000;
+            string oceanSizeUnits = "square miles";
+
+            string pacificInfo = $"The {ocean} is {oceanSize} {oceanSizeUnits}.";
+            //"The Pacific Ocean is 6800000 square miles."
+            if (answer.Contains("pacific")) 
+            {
+                MessageBox.Show("Correct!", "Result");
+            }
+            else 
+            {
+                MessageBox.Show("Incorrect, the answer is the Pacific Ocean", "Result");
+            }
+         
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
